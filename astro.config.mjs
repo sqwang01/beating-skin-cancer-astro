6ce3f9fdd009533123540528dd312830cba24f08
Your Name <your.email@example.com>
2026-08-22 00:13:43 -0700

    Initial Astro migration

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.beatingskincancer.com',
+  trailingSlash: 'never',
+  integrations: [icon(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
